2deb6f1785435ca46e8b840eba9f047a823b059d
DaSherman0210 <tgerman181@gmail.com>
2023-07-13 19:05:15 -0500

    feat: :rocket: Page cosume and API
    Page who is going to consume a API through cards from MongoDB database

---
+++ frontend/index.js
@@ -0,0 +1,27 @@
+const cartasContainer = document.querySelector('.cartas');
+
+cartasContainer.addEventListener('click', (event) => {
+  const target = event.target;
+  
+  if (target.classList.contains('like')) {
+    if (target.classList.contains('loke', 'fa-solid', 'fa-thumbs-up', 'fa-bounce')) {
+      target.classList.remove('loke' , 'fa-bounce');
+      target.classList.add('like');
+    }
+     else {
+      target.classList.add('loke', 'fa-solid', 'fa-thumbs-up', 'fa-bounce');
+    }
+  }
+  if (target.classList.contains('loke', 'fa-solid', 'fa-thumbs-up', 'fa-bounce')) {
+    
+  }
+
+  if (target.classList.contains('dislike')) {
+    if (target.classList.contains('disloke', 'fa-solid', 'fa-thumbs-down', 'fa-bounce')) {
+      target.classList.remove('disloke' , 'fa-bounce');
+      target.classList.add('dislike');
+    } else {
+      target.classList.add('disloke', 'fa-solid', 'fa-thumbs-down', 'fa-bounce');
+    }
+  }
+});
